860964bf1afd4df1c63d9354bfc87d6fa427867f
filoupegase <loisoncorentin@yahoo.fr>
2022-01-23 20:02:12 +0100

    push

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router/index.jsx';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
 
 ReactDOM.render(
