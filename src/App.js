6af8494bbd0f9c23fb10a6a82d7dd050e137d51a
filoupegase <loisoncorentin@yahoo.fr>
2022-01-23 18:46:24 +0100

    feat/init-react-router-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as ReactRouter, Navigate, Route, Routes } from 'react-router-dom';
+import DataGridEmployee from '../src/pages/DataGridEmployee';
+import CreateEmployee from '../src/pages/CreateEmployee';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <ReactRouter>
+          <Routes>
+            <Route path="/"
+                   element={ <CreateEmployee /> }
+                   strict exact />
+            <Route path="/employee-grid"
+                   element={ <DataGridEmployee /> }
+                   strict exact />
+            <Route path="*" element={
+              <Navigate to="/" /> } strict exact />
+          </Routes>
+        </ReactRouter>
+      </>
   );
 }
 
---
+++ src/pages/CreateEmployee/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const CreateEmployee = () => {
+  return (
+      <p>CreateEmployee</p>
+  );
+};
+
+export default CreateEmployee;
