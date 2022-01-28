fad411f2144831ecb5c49c8b3f4a46c9b77a8166
filoupegase <loisoncorentin@yahoo.fr>
2022-01-28 14:38:18 +0100

    feat/change event change

--- src/components/CreateEmployeeForm/index.jsx
+++ src/components/CreateEmployeeForm/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
+
+const useStyle = makeStyles(() => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+}));
 
 const CreateEmployeeForm = () => {
+  const classes = useStyle();
+  const [firstName, setFirstName] = useState('');
+  console.log(firstName);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (e, set) => {
+    set(e.target.value);
+  };
+
   return (
-      <>
+      <form
+          onSubmit={ handleSubmit }
+          className={ classes.form }
+      >
         <TextField
-            id="outlined-basic"
-            label="Outlined"
+            type="text"
+            id="firstname"
+            label="FirstName"
             variant="outlined"
+            value={ firstName }
+            onChange={
+              (e) => handleChange(e, setFirstName) }
         />
         <TextField
             id="outlined-basic"
@@ -25,7 +52,7 @@ const CreateEmployeeForm = () => {
             label="Outlined"
             variant="outlined"
         />
-      </>
+      </form>
   );
 };
 
